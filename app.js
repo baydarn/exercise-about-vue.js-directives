0582497cda668f7d1fb5fb2d9a6143008341daca
Neslin Baydar <37474673+baydarn@users.noreply.github.com>
2020-12-23 00:01:20 +0300

    added exercise

---
+++ app.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#exercise',
+    data: {
+    show:false,
+      array: ['X', 'Y', 'Z', 'T'],
+      customObject: {
+        title: 'Game of Thrones',
+        author: 'George R.R',
+        books: '7'
+      },
+      testData: {
+        name: 'NesnedLoop', 
+        id: 19,
+        data: [4.3, 2.67, 10.42, 1402]
+      },
+     
+    }
+  });
+  
